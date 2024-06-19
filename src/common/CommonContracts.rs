7297e2e803e089965c1675deee3b2f7e6f581609
mgkang <mgkang@babkorea.com>
2024-06-19 11:38:39 +0900

    입력할 수 있게 수정

---
+++ src/common/CommonContracts.rs
@@ -0,0 +1,4 @@
+pub mod Sentence {
+    pub const FIX_KOREAN: &str = "(질문하기 앞서 한글로 대답해줘.)";
+    pub const QUESTION:& str = "질문을 입력해주세요.";
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,7 @@
+mod common;
+
+use std::io;
+use common::CommonContracts;
 use genai::{
     chat::{ChatMessage, 
         ChatRequest
@@ -12,16 +16,21 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     let mut chat_req = ChatRequest::default().with_system("Answer with one sentence.");
 
+    println!("{}", CommonContracts::Sentence::QUESTION);
+
+    let mut input = String::new();
+    io::stdin().read_line(&mut input).expect("Failed to read line");
+    
     let questions = [
         // --
-        "러스트 언어로 웹소켓 생성하는 예제코드 보여줘. 한글로 보여줘.",
+        input.trim(),
     ];
 
     let model = "llama3"; // LM Studio 다운로드 후, http://localhost:11434
 
     for question in questions {
         println!("\n-- Question:\n{question}");
-        chat_req = chat_req.append_message(ChatMessage::user(question));
+        chat_req = chat_req.append_message(ChatMessage::user(format!("{} {}", CommonContracts::Sentence::FIX_KOREAN, question)));
 
         println!("\n-- Answer (from {model})");
         let chat_res = client.exec_chat_stream(model, chat_req.clone(), None).await?;
@@ -31,6 +40,15 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     }
 
     // Multi-AI
+    // let client = Client::default();
+
+    // let mut chat_req = ChatRequest::default().with_system("Answer with one sentence.");
+
+    // let questions = [
+    //     // --
+    //     "rust lang으로 main.rs 에서 run 시, 입력값을 넣을 수 있게 예제코드 보여줘",
+    // ];
+
     // let models = ["llama3", "gpt-3.5-turbo"];
 
     // for (question, model) in questions.into_iter().zip(models) {
@@ -44,5 +62,5 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     //     chat_req = chat_req.append_message(ChatMessage::assistant(response));
     // }
 
-    Ok(())
+    Ok(().into())
 }
